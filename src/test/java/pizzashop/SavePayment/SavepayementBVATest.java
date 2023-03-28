3292154561a33b5ace3bd788dca8e6f9e1f9b824
baciu777 <72201455+baciu777@users.noreply.github.com>
2023-03-28 20:54:59 +0300

    add bva tests

--- src/test/java/pizzashop/SavePayment/SavepayementBVATest.java
+++ src/test/java/pizzashop/SavePayment/SavepayementBVATest.java
@@ -1,8 +1,8 @@
 package pizzashop.SavePayment;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import pizzashop.model.Payment;
 import pizzashop.model.PaymentType;
 import pizzashop.repository.PaymentRepository;
@@ -31,11 +31,13 @@ class SavepayementBVATest {
         reader.close();
     }
 
-    @Test
-    void testValid() throws IOException {
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 2, 8 })
+    @Tag("valid")
+    void testValid(Integer tableNumber) throws IOException {
         //Arrange
-        int tableNumber = 1;
-        PaymentType type = PaymentType.CARD;
+
+        PaymentType type = PaymentType.CASH;
         double amount = 123.0;
         Payment payment=new Payment(tableNumber,type,amount);
         //Act
@@ -52,12 +54,48 @@ class SavepayementBVATest {
         }
         assertTrue(found);
     }
+    @ParameterizedTest
+    @ValueSource(ints = {8 })
+    @Tag("valid")
+    void testValid2(Integer tableNumber) throws IOException {
+        //Arrange
 
-    @Test
+        PaymentType type = PaymentType.CASH;
+        double amount = 1;
+        Payment payment=new Payment(tableNumber,type,amount);
+        //Act
+        paymentRepository.add(payment);
+
+        //Assert
+        String line;
+        boolean found = false;
+        while ((line = reader.readLine()) != null) {
+            if (line.contains(tableNumber + "," + type + "," + amount)) {
+                found = true;
+                break;
+            }
+        }
+        assertTrue(found);
+    }
+    @RepeatedTest(value = 5, name = "{displayName} {currentRepetition}/{totalRepetitions}")
+    @DisplayName("InvalidTest")
+    @Tag("invalid")
     void testInvalid() {
         int tableNumber = 0;
         PaymentType type = PaymentType.CASH;
         double amount = 50.0;
+        Payment payment = new Payment(tableNumber, type, amount);
+        assertThrows(IllegalArgumentException.class, () -> paymentRepository.add(payment));
+    }
+
+    @RepeatedTest(value = 5, name = "{displayName} {currentRepetition}/{totalRepetitions}")
+    @DisplayName("InvalidTest")
+    @Timeout(2)
+    @Tag("invalid")
+    void testInvalid2() {
+        int tableNumber = 7;
+        PaymentType type = PaymentType.CARD;
+        double amount = 0.99;
         Payment payment = new Payment(tableNumber, type, amount);
         assertThrows(IllegalArgumentException.class, () -> paymentRepository.add(payment));
     }
